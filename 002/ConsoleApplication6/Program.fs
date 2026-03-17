eede346555e7038ba7dd037e1b608fe06b753f1a
ElleBeders <malefisenta135@gmail.com>
2026-03-17 06:15:26 +0500

    Add files via upload

---
+++ 002/ConsoleApplication6/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+// Фильтр
+let makeEvenNumber (digits: seq<char>) =
+    let evenChars = Seq.filter (fun c -> c = '0' || c = '2' || c = '4' || c = '6' || c = '8') digits
+    
+    
+    if Seq.isEmpty evenChars then 0 // Чётных нет — 0
+    else
+        Seq.fold (fun acc c ->
+            let digit = int c - int '0'  // Преобразуем тип
+            acc * 10 + digit   // Добавляем новую цифру
+        ) 0 evenChars
+
+// Основная программа
+[<EntryPoint>]
+let main argv =
+    printfn "Введите последовательность цифр (например, 123):"
+    let input = Console.ReadLine()  // Читаем строку 
+    
+    let result = makeEvenNumber input
+    
+    printfn "Число из чётных цифр: %d" result
+    0  
